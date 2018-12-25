fb72d34151faafec7c8f274489ca53812b3b06c9
Synacten <blackhorns9999@gmail.com>
2018-12-25 21:58:43 +0200

    add animation

--- src/components/pages/Figure.js
+++ src/components/pages/Figure.js
@@ -3,13 +3,28 @@ import styled from 'styled-components'
 import {NavBar} from '../layouts/NavBar'
 
 export class Figure extends Component {
+    constructor() {
+        super();
+        this.state = {
+            frame: 0
+        }
+
+        setInterval(this.nextFrame.bind(this), 25)
+    }
+
+    nextFrame() {
+        const {frame} = this.state
+
+        this.setState({ frame: frame + 1});
+    }
   render() {
+      const { frame } = this.state;
       const size = 900;
       const padding = 20;
       const stroke = 1;
       const d = new Array(1100).fill(0).reduce((result, $, index) => {
-        const x = (size / 4 - padding) * Math.cos(2 * index);
-        const y = (size / 2 - padding) * Math.tan(.2 * index);
+        const x = (size / 4 - padding) * Math.sin(2 * index + frame / 620);
+        const y = (size / 2 - padding) * Math.tan(.2 * index + frame / 500);
         const c = index === 0 ? 'M' : 'L';
 
         return result + `${c}${x + size / 2} ${y + size / 2}`
