49670068609ad67a53af61f097def0f59ebbbadd
Andrey <blackhorns9999@gmail.com>
2018-12-28 17:26:34 +0200

    add animation with sc

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,6 @@ const Router = require('koa-router');
 const router = new Router();
 const {testapi} = require('../controllers/notice')
 const {formAuth} = require('../controllers/auth')
-const bcrypt = require('bcrypt')
 
   //routes
   router.get('/testapi', testapi)
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,17 +8,53 @@ export class About extends Component {
     this.state = {
       borderWidth: 5,
       size: 1200,
-      gridColor: 'grey'
+      gridColor: 'grey',
+      PositionX: 50,
+      PositionY: 50,
+      rotateR: 0,
     }
   }
 
-  handleChange = () => {
+  borderChange = (event) => {
     this.setState({borderWidth: event.target.value});
+  }
+
+  xPosition = (event) => {
+    this.setState({PositionX: event.target.value});
+  }
+
+  yPosition = (event) => {
+    this.setState({PositionY: event.target.value});
+  }
+
+  rRotate = (event) => {
+    this.setState({rotateR: event.target.value});
   }
 
   render() {
     const d = "M100 85 L100 10 L410 10 L410 370 L100 370 L100 85 L180 85 L385 290 L385 10"
-    const { borderWidth, size, gridColor } = this.state
+    const { borderWidth, size, gridColor, PositionX, PositionY, rotateR } = this.state;
+    const _SvgBlock = styled.div`
+      .figure{
+      position: absolute;
+      top: ${this.state.PositionY}px;
+      left: ${this.state.PositionX}px;
+      transform: rotate3d(1, 2, 1, ${this.state.rotateR}deg);
+      z-index: 5;
+      }
+      .X{
+      position: absolute;
+      top: 70%;
+      left: 50px;
+      z-index: 4
+      }
+      .Y{
+      position: absolute;
+      top: 10%;
+      left: 50px;
+      z-index: 4
+      }
+      `
     document.title = 'About'
     return (
       <div>
@@ -39,7 +75,7 @@ export class About extends Component {
               stroke={gridColor}
               strokeWidth='2px'
               fill='transparent'
-            ></path>
+              ></path>
           </svg>
           <svg className='Y' height={size} width={size}>
             <path
@@ -47,14 +83,26 @@ export class About extends Component {
               stroke={gridColor}
               strokeWidth='2px'
               fill='transparent'
-            ></path>
+              ></path>
           </svg>
           </_SvgBlock>
           <_FormBlock>
             <form>
               <label>
                 BorderChange:
-                <input type="number" value={borderWidth} onChange={this.handleChange} />
+                <input type="number" value={borderWidth} onChange={this.borderChange} />
+              </label>
+              <label>
+                X-position:{PositionX}
+                <input type="range" min='-900' max='900' value={PositionX} onChange={this.xPosition} />
+              </label>
+              <label>
+                Y-position:{PositionY}
+                <input type="range" min='-900' max='900' value={PositionY} onChange={this.yPosition} />
+              </label>
+              <label>
+                Rotate-Right:{rotateR}*
+                <input type="range" min='0' max='180' value={rotateR} onChange={this.rRotate} />
               </label>
             </form>
           </_FormBlock>
@@ -63,32 +111,14 @@ export class About extends Component {
   }
 }
 
-const _SvgBlock = styled.div`
-  .figure{
-    position: absolute;
-    top: 30%;
-    left: 50px;
-    z-index: 1;
-  }
-  .X{
-    position: absolute;
-    top: 70%;
-    left: 50px;
-    z-index: 4
-  }
-  .Y{
-    position: absolute;
-    top: 10%;
-    left: 50px;
-    z-index: 4
-  }
-`
+
+
 
 const _FormBlock = styled.div`
   border-left: 1px solid green;
   width: 100px;
   height: 100%;
-  position: absolute;
+  position: fixed;
   right: 100px;
   top: 50px;
   form{
